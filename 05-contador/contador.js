05a321d9e25fcc905832d426f719839b04ccdd05
FERNANDO DE SÁ SOUZA <25203836@CFP127ALUNO.LOCAL>
2026-03-30 16:04:49 -0300

    Realizada as funções em JS para o Contador

--- 05-contador/contador.js
+++ 05-contador/contador.js
@@ -1,35 +1,75 @@
 'use strict'
 
-const criarListaNumeros = function(quantidade){
-    let listaNumeros = []
-    for(let i = 1; i <= quantidade; i++){
-        listaNumeros.push(i)
-    }
-    return listaNumeros
-}
+document.getElementById("btnGerar").onclick = function() {
+    let quantidade = document.getElementById("quantidade").value
+    quantidade = Number(quantidade)
 
-const numerosPares = function(quantidade){
-    let listaPar = []
+    if(!quantidade || isNaN(quantidade) || quantidade <= 0){
+        alert("Digite uma quantidade válida")
+        return
+    }
+
+    if(quantidade > 100){
+        alert("Máximo permitido é 100!")
+        return
+    }
+
+    const par = function(n){
+        return n % 2 === 0
+    }
+
+    const multiploDe5 = function(n){
+        return n % 5 === 0
+    }
+
+    const potencia2 = function(n){
+        return n * n
+    }
+
+    let numeros = []
+    let pares = []
+    let impares = []
+    let multiplos5 = []
+    let potencias = []
+
     for(let i = 1; i <= quantidade; i++){
-        if(i % 2 === 0){
-           listaPar.push(i)
+        numeros.push(i)
+
+        if(par(i)){
+            pares.push(i)
+        }else{
+            impares.push(i)
         }
-    }
-    return listaPar
-}
-
-const numerosImpares = function(quantidade){
-    let listaImpar = []
-    for(let i = 1; i <= quantidade; i++){
-        if(i % 2 !== 0){
-            listaImpar.push(i)
+        if(multiploDe5(i)){
+            multiplos5.push(i)
         }
+        potencias.push(potencia2(i))
     }
-    return listaImpar
+
+    let tabela = document.getElementById("tabelaBody")
+
+    tabela.innerHTML = ""
+
+    let max = Math.max(
+        numeros.length,
+        pares.length,
+        impares.length,
+        multiplos5.length, 
+        potencias.length
+    )
+
+    for(let i = 0; i < max; i++){
+        let linha = "<tr>"
+
+        linha += "<td>" + (numeros[i] || "") + "</td>"
+        linha += "<td>" + (pares[i] || "") + "</td>"
+        linha += "<td>" + (impares[i] || "") + "</td>"
+        linha += "<td>" + (multiplos5[i] || "") + "</td>"
+        linha += "<td>" + (potencias[i] || "") + "</td>"
+
+        linha += "</tr>"
+
+        tabela.innerHTML += linha
+    }
 }
 
-const criarLinha = function(num, par, impar, mult5, pot2){
-    
-}
-console.log(numerosImpares(10))
-//console.log(criarListaNumeros)
